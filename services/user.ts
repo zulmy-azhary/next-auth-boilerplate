2a9dd4c2ae158da30fde3921a10f20a8d6fbbda7
zoel-a <zulmyazhary32@gmail.com>
2024-01-09 14:11:13 +0800

    Create user services

---
+++ services/user.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { registerSchema } from "@/schemas";
+import { z } from "zod";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { email } });
+
+    return user;
+  } catch {
+    return null;
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } });
+
+    return user;
+  } catch {
+    return null;
+  }
+};
+
+export const createUser = async (payload: z.infer<typeof registerSchema>) => {
+  try {
+    return await db.user.create({
+      data: payload
+    })
+  } catch {
+    return null;
+  }
+}
