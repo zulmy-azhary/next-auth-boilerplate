78e0e2038730b291773d9d96e9b7e1ca53b60578
zoel-a <zulmyazhary32@gmail.com>
2024-01-09 14:02:45 +0800

    Create route rules

---
+++ routes.ts
@@ -0,0 +1,11 @@
+export const publicRoutes: string[] = [];
+
+export const authRoutes: string[] = [
+  "/login",
+  "/register",
+  "/error"
+];
+
+export const apiAuthPrefix: string = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT: string = "/";
