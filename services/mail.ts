dc8be4946b46b458c3e77334307691019a5d3e05
zoel-a <zulmyazhary32@gmail.com>
2024-01-11 22:59:55 +0800

    Update verification email path

--- services/mail.ts
+++ services/mail.ts
@@ -3,12 +3,12 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const verifyEmailLink = `http://localhost:3000/verify?token=${token}`;
 
   await resend.emails.send({
     from: "nextdashboard@resend.dev",
     to: email,
     subject: "Confirm your email",
-    html: `<p>Click <a href="${confirmLink}">Here</a> to confirm your email.</p>`,
+    html: `<p>Click <a href="${verifyEmailLink}">Here</a> to confirm your email.</p>`,
   });
 };
