b5df51a572a0ed082c2890a5a50bc34cb2b88595
zoel-a <zulmyazhary32@gmail.com>
2024-01-13 20:51:01 +0800

    Refactor error card

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -1,12 +1,30 @@
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { AuthError } from "next-auth";
+import { redirect } from "next/navigation";
 
-export const ErrorCard = () => {
+type ErrorCardProps = {
+  message?: AuthError["type"];
+};
+
+export const ErrorCard = ({ message }: ErrorCardProps) => {
+  let headerDescription =
+    "Oops! Something went wrong. Please contact administrator for more details or try again later.";
+
+  if (!message) {
+    redirect("/login");
+  }
+
+  if (message === "OAuthAccountNotLinked") {
+    headerDescription =
+      "Another account already registered with the same Email Address. Please login the different one.";
+  }
+
   return (
     <CardWrapper
-      headerTitle="Error"
-      headerDescription="Oops! Something went wrong!"
+      headerTitle="An Error Occured"
+      headerDescription={headerDescription}
       backButtonLabel="Back to login"
       backButtonHref="/login"
-     />
+    />
   );
 };
