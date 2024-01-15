36b4ad33bca3bbd8a7e6dfa7b3be8c4e61cf4f54
zoel-a <zulmyazhary32@gmail.com>
2024-01-16 01:07:48 +0800

    Create 2fa token services

---
+++ services/two-factor-token.ts
@@ -0,0 +1,59 @@
+import { db } from "@/lib/db";
+import { setTokenExpiration } from "@/lib/utils";
+import crypto from "node:crypto";
+
+export const generateTwoFactorToken = async (email: string) => {
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const token = String(crypto.randomInt(100000, 1000000));
+  const expires = setTokenExpiration(60 * 2); // 2 minutes
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
+};
+
+export const getTwoFactorToken = async (token: string) => {
+  try {
+    const twoFactorToken = await db.twoFactorToken.findUnique({
+      where: { token },
+    });
+
+    return twoFactorToken;
+  } catch {
+    return null;
+  }
+};
+
+export const getTwoFactorTokenByEmail = async (email: string) => {
+  try {
+    const twoFactorToken = await db.twoFactorToken.findFirst({
+      where: { email },
+    });
+
+    return twoFactorToken;
+  } catch {
+    return null;
+  }
+};
+
+export const deleteTwoFactorTokenById = async (id: string) => {
+  try {
+    return await db.twoFactorToken.delete({
+      where: { id },
+    });
+  } catch {
+    return null;
+  }
+};
