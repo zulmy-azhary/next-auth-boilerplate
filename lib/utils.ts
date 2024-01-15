fc50860176d5f69e61d01faf5c549c9f0b2da015
zoel-a <zulmyazhary32@gmail.com>
2024-01-16 01:09:32 +0800

    Add utils to handle expiration and jwt

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,54 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { sign, verify, type SignOptions, type Secret } from "jsonwebtoken";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * Function to check whether the given value is expired or not.
+ * @param expires The date that want to check
+ * @return true if the value is expired, false otherwise
+ */
+export function isExpired(expires: Date): boolean {
+  return new Date(expires) < new Date();
+}
+
+/**
+ * Function to set token expiration.
+ * @param exp Duration of token expiration, default is 3600 milliseconds or 1 hour
+ * @return Generates datetime for the token expiration
+ */
+export function setTokenExpiration(exp: number = 60 * 60) {
+  return new Date(new Date().getTime() + 1000 * exp);
+}
+
+/**
+ * Function to generate jwt.
+ * @param payload The payload want to generate
+ * @param options The sign options
+ * @return The token generated
+ */
+
+export function signJwt(payload: Record<string, unknown>, options?: SignOptions) {
+  return sign(payload, process.env.JWT_SECRET as Secret, {
+    ...options,
+    algorithm: "HS256",
+  });
+}
+
+export const verifyJwtToken = <T extends object>(token: string) => {
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET as Secret);
+    return {
+      valid: true,
+      decoded: decoded as T,
+    };
+  } catch (error) {
+    return {
+      valid: false,
+      decoded: null,
+    };
+  }
+};
