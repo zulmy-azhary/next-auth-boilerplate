f39256e2dad0f1da36465da16b1bc4e080938c89
zoel-a <zulmyazhary32@gmail.com>
2024-01-16 01:08:26 +0800

    Create two factor confirmation services

---
+++ services/two-factor-confirmation.ts
@@ -0,0 +1,42 @@
+import { db } from "@/lib/db";
+import { setTokenExpiration } from "@/lib/utils";
+
+export const generateTwoFactorConfirmation = async (userId: string) => {
+  const existingTwoFactorConfirmation = await getTwoFactorConfirmationByUserId(userId);
+  if (existingTwoFactorConfirmation) {
+    await deleteTwoFactorConfirmationById(existingTwoFactorConfirmation.id);
+  }
+
+  const expires = setTokenExpiration(60 * 15); // 15 minutes
+
+  const twoFactorConfirmation = await db.twoFactorConfirmation.create({
+    data: {
+      userId,
+      expires,
+    },
+  });
+
+  return twoFactorConfirmation;
+};
+
+export const getTwoFactorConfirmationByUserId = async (userId: string) => {
+  try {
+    const twoFactorConfirmation = await db.twoFactorConfirmation.findUnique({
+      where: { userId },
+    });
+
+    return twoFactorConfirmation;
+  } catch {
+    return null;
+  }
+};
+
+export const deleteTwoFactorConfirmationById = async (id: string) => {
+  try {
+    return await db.twoFactorConfirmation.delete({
+      where: { id },
+    });
+  } catch {
+    return null;
+  }
+};
