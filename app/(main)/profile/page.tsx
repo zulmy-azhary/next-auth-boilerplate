4d1e00d0651901ad1be9b0bc4d7a1aecb4ba54c4
zoel-a <zulmyazhary32@gmail.com>
2024-01-28 18:37:06 +0800

    Update profile page

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,9 +1,19 @@
+import { ProfileForm } from "@/components/form/profile-form";
+import { currentUser } from "@/lib/auth";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Profile",
 };
 
-export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+export default async function ProfilePage() {
+  const user = await currentUser();
+  if (!user) return;
+  
+  return (
+    <div className="grid grid-cols-7 gap-y-14">
+      <h2 className="text-xl col-span-full col-start-4 font-semibold">Profile Settings</h2>
+      <ProfileForm user={user} />
+    </div>
+  );
 }
