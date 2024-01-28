64c19e7536895e577232306299556c725184787a
zoel-a <zulmyazhary32@gmail.com>
2024-01-28 18:37:25 +0800

    Create settings page

---
+++ app/(main)/settings/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Settings",
+};
+
+export default function SettingsPage() {
+  return (
+    <div>SettingsPage</div>
+  )
+}
