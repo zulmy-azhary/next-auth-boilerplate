3ed19ef5e9dc807822112788c36afb4e358cec14
zoel-a <zulmyazhary32@gmail.com>
2024-01-30 15:38:15 +0800

    refactor: change db provider

--- services/mail.ts
+++ services/mail.ts
@@ -6,7 +6,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const verifyEmailLink = `${process.env.NEXT_PUBLIC_APP_URL}/verify?token=${token}`;
 
   await resend.emails.send({
-    from: `mail@${process.env.RESEND_DOMAIN}`,
+    from: process.env.EMAIL_FROM as string,
     to: email,
     subject: "[Next Dashboard] Action required: Verify your email",
     html: `<p>Click <a href="${verifyEmailLink}">Here</a> to verify your email.</p>`,
@@ -17,7 +17,7 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
   const resetPasswordLink = `${process.env.NEXT_PUBLIC_APP_URL}/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: `mail@${process.env.RESEND_DOMAIN}`,
+    from: process.env.EMAIL_FROM as string,
     to: email,
     subject: "[Next Dashboard] Action required: Reset your password",
     html: `<p>Click <a href="${resetPasswordLink}">Here</a> to reset your password.</p>`,
@@ -26,7 +26,7 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: `mail@${process.env.RESEND_DOMAIN}`,
+    from: process.env.EMAIL_FROM as string,
     to: email,
     subject: "[Next Dashboard] Action required: Confirm Two-Factor Authentication",
     html: `<p>${token} is your authentication Code.</p>`,
